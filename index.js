14f45cb264e47c0b40196b0cf31125e31d5725aa
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-06-30 18:08:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,9 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string){
+  if(string === string.toLowerCase())
+    return "I can\'t hear you!";
+  else if(string === string.toUpperCase())
+    return "YES INDEED!";
   
 }
